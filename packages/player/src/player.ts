1a9eecee3abd06575f52d1ff20415010c2d91b88
Raymond Lucke <ray@raylucke.com>
2026-09-08 06:21:52 -0700

    release: 0.5.9

--- packages/player/src/player.ts
+++ packages/player/src/player.ts
@@ -276,7 +276,7 @@ interface MigrationTxn {
  */
 export class MoqtPlayer {
   /** Player version (set at build time). */
-  static readonly version = '0.5.8';
+  static readonly version = '0.5.9';
 
   private readonly config: MoqtPlayerConfig;
   private readonly emitter = new TypedEmitter<PlayerEventMap>();
